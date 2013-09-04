0668949d108c98d1af27eb592ecfd43cb6d96efb
Brice Texier <burisu@oneiros.fr>
2013-09-04 14:04:42 +0200

    MAke process working for thumbnails

--- app/assets/images/screenshots/process.sh
+++ app/assets/images/screenshots/process.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-for file in ls 
+
+for file in `ls originals/*.png`
+do
+    thumbnail=thumbnails/`basename $file`
+    echo "$file => $thumbnail"
+    mkdir -p `dirname $thumbnail`
+    convert $file -geometry 1000x200 $thumbnail
+done
