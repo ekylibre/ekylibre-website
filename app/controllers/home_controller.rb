f444f31333f67d1d7f4d38651e110ee4669e5fde
Brice TEXIER <brice@ekylibre.com>
2015-07-02 14:31:01 +0200

    Fixes invalid RSS fetch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,25 +4,33 @@ class HomeController < ApplicationController
     @video = Rails.cache.read(:video)
     unless @video
       feed = 'https://gdata.youtube.com/feeds/api/users/UC_yYJGkq-aqC-So8DlXtM5g/uploads'
-      rss = Feedjira::Feed.fetch_and_parse(feed)
-      entry = rss.entries.first
-      parameters = CGI::parse(URI(entry.url).query)
-      @video = parameters["v"].first
-      Rails.cache.write(:video, @video, timeToLive: 1.day)
+      begin
+        rss = Feedjira::Feed.fetch_and_parse(feed)
+        entry = rss.entries.first
+        parameters = CGI::parse(URI(entry.url).query)
+        @video = parameters["v"].first
+        Rails.cache.write(:video, @video, time_to_live: 1.day)
+      rescue Feedjira::FetchFailure => e
+        Rails.logger.warn "Cannot fetch youtube data"
+      end
     end
 
     @articles = Rails.cache.read(:articles)
     unless @articles
       feed = 'http://forum.ekylibre.org/c/ekylibre/news.rss'
-      rss = Feedjira::Feed.fetch_and_parse(feed)
-      @articles = Array.new
-      5.times do |i|
-        item, article = rss.entries[i], Hash.new
-        article[:title], article[:url] = item.title, item.url
-        article[:published] = item.published.to_date
-        @articles << article
+      begin
+        rss = Feedjira::Feed.fetch_and_parse(feed)
+        @articles = Array.new
+        5.times do |i|
+          item, article = rss.entries[i], Hash.new
+          article[:title], article[:url] = item.title, item.url
+          article[:published] = item.published.to_date
+          @articles << article
+        end
+        Rails.cache.write(:articles, @articles, timeToLive: 15.minutes)
+      rescue Feedjira::FetchFailure => e
+        Rails.logger.warn "Cannot fetch forum data"
       end
-      Rails.cache.write(:articles, @articles, timeToLive: 15.minutes)
     end
   end
 
